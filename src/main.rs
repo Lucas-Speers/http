59ae4c6b339b904ecb26b85de6e1bb6fe2a617ef
Lucas Speers <lucas_speers@outlook.com>
2024-09-09 10:51:46 -0600

    reading settings.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Settings {
+    port: u16,
+    ip: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read("settings.toml")
+        .unwrap()
+        .iter()
+        .map(|c| *c as char)
+        .collect::<String>();
+
+    let settings: Settings = toml::from_str(&file).unwrap();
+    
+    println!("{:?}", settings);
 }
